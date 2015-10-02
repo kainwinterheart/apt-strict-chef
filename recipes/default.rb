0c2fd94d38ec63db92b89143686b611a2f72e4ff
Gennadiy Filatov <gfilatov@cpan.org>
2015-10-02 17:10:58 +0300

    Do it as early as possible

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,21 +2,21 @@ directory "/usr/share/apt_strict" do
     owner "root"
     group "root"
     mode '0755'
-    action :create
-end
+    action :nothing
+end.run_action( :create )
 
 cookbook_file "/usr/share/apt_strict/executable.py" do
     source "executable.py"
     owner "root"
     group "root"
     mode '0755'
-    action :create_if_missing
-end
+    action :nothing
+end.run_action( :create_if_missing )
 
 cookbook_file "/usr/share/apt_strict/apt_strict.py" do
     source "apt_strict.py"
     owner "root"
     group "root"
     mode '0644'
-    action :create_if_missing
-end
+    action :nothing
+end.run_action( :create_if_missing )
